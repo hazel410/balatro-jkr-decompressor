1dc83c685b9545f7c366b8ccb4bce80d0b11c00a
hazel410 <wild.hazelnut.tree@gmail.com>
2025-07-06 21:36:20 -0400

    "started trying to fix this again-- still does some weird shit

--- profile_reader.py
+++ profile_reader.py
@@ -5,39 +5,77 @@ decompressed_path = 'decompressed/profile_decoded.txt'
 # opening file and storing the data
 with open(decompressed_path, 'r') as file:
   raw_txt = file.read()
+raw_txt = raw_txt[(raw_txt.find("{")+1):raw_txt.rfind("}")]
 
-# indexing flags
-flags = [
-  "[\"career_stats\"]",
-  "[\"all_unlocked\"]",
-  "[\"stake\"]",
-  "[\"joker_usage\"]",
-  "[\"deck_stakes\"]",
-  "[\"voucher_usage\"]",
-  "[\"challenge_progress\"]",
-  "[\"MEMORY\"]",
-  "[\"hand_usage\"]",
-  "[\"progress\"]",
-  "[\"deck_usage\"]",
-  "[\"consumable_usage\"]",
-  "[\"high_scores\"]",
-  "[\"name\"]",
-  "[\"challenges_unlocked\"]"
-]
+# initializing loop vars
+left_bbracket = 0
+right_bbracket = 0
+left_cbracket = 0
+right_cbracket = 0
+section_keys = []
+section_text = []
+temp_list = []
 
-# raw strings
-string_list = [None] * len(flags)
-for flag_num in range(len(flags)):
-  start_str = raw_txt.find(flags[flag_num])
-  if flag_num < 14:
-    string_list[flag_num] = raw_txt[start_str:raw_txt.find(flags[flag_num+1])]
-  else:
-    string_list[flag_num] = raw_txt[start_str:]
-print(raw_txt.find(flags[3]))
-print(raw_txt.find(flags[4]))
-print(string_list[3])
 
-# ["stake"] isnt being properly found-- idk what memory is doing
+def list_to_word(input_list):
+  temp_output = ""
+  for character in input_list:
+    if character != "\"":
+      temp_output = temp_output + character
+  return temp_output
+
+for char in raw_txt:
+  # updating counts of brackets
+  if char == "{":
+    left_cbracket += 1
+  elif char == "}":
+    right_cbracket += 1
+  elif char == "[":
+    left_bbracket += 1
+  elif char == "]":
+    right_bbracket += 1
+  
+  # if we're inside box brackets that aren't nested in curly brackets, log the text
+  if left_cbracket == right_cbracket and left_bbracket > right_bbracket:
+    temp_list.append(char)
+  
+  # if we're inside un-nested cbrackets log the text
+  if left_cbracket > right_cbracket:
+    temp_list.append(char)
+  
+  # for the edge case of the format "[example_key]=20" (notice the lack of cbrackets)
+  if left_bbracket == right_bbracket and left_cbracket == right_cbracket:
+    temp_list.append(char)
+  
+  # if we're closing an un-nested curly bracket, log it
+  if char == "}" and left_cbracket == right_cbracket:
+    section_text.append(list_to_word(temp_list))
+    temp_list = []
+  
+  # if we're closing an un-nested box bracket log it
+  if char == "]" and left_cbracket == right_cbracket:
+    section_keys.append(list_to_word(temp_list[1:]))
+    temp_list = []
+  
+  # for the edge case
+  if char == "{" and left_bbracket == right_bbracket and left_cbracket == right_cbracket + 1:
+    section_text.append(list_to_word(temp_list))
+    temp_list = []
+  
+
+# hopefully everything works now
+# print(section_keys)
+
+# print(len(section_keys))
+# print(len(section_text))
+
+  
+  
+  
+
+
+
+
 
 
 
